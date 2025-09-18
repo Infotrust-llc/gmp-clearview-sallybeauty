bc913f0e25d3c9dd997f755297190eb079f56a95
Alina Zilbergerts <alina.zilbergerts@infotrustllc.com>
2025-09-18 17:05:44 +0000

    add date range for backfill

--- definitions/core/01_sources/declarations.js
+++ definitions/core/01_sources/declarations.js
@@ -4,11 +4,6 @@
   Alina Zilbergerts, Trish Dothkar,
   -- */
 
-// declare({
-//     database: dataform.projectConfig.defaultProject,
-//     schema: dataform.projectConfig.vars.GA4_DATASET,
-//     name: 'events_2025*'
-// });
 declare({
     database: dataform.projectConfig.defaultProject,
     schema: dataform.projectConfig.vars.GA4_DATASET,
@@ -18,13 +13,11 @@ declare({
     database: dataform.projectConfig.defaultProject,
     schema: dataform.projectConfig.vars.GA4_DATASET,
     tags: ["prod"],
+    name: 'events_intraday_*'
+});
+declare({
+    database: dataform.projectConfig.defaultProject,
+    schema: dataform.projectConfig.vars.GA4_DATASET,
+    tags: ["prod"],
     name: 'events_fresh_*'
 });
-
-
-// ["ga4_sessions", "ga4_pages", "ga4_products", "ga4_events"]
-//   .forEach(source => declare({
-//       schema: "gmp_output",
-//       name: source
-//     })
-//   );
