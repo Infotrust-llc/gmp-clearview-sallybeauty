e26aafc0e211376a86a1f35bee78b55d580c81c7
Alina Zilbergerts <alina.zilbergerts@infotrustllc.com>
2025-10-16 00:45:09 +0000

    backfill channel data for October 2024

--- includes/core/helpers.js
+++ includes/core/helpers.js
@@ -689,7 +689,120 @@ function generateAlterTableStatements(tables) {
     // Join all statements with line breaks to form the full script
     .join("\n\n");
 }
+;
 
+
+/* Function #1
+ * Determines the default channel grouping for a given session based on source, medium, and campaign
+ * based on combining google_ads_campaign and manual_campaign from session_traffic_source_last_click field
+ *
+ * @param {string} source - The traffic source (e.g., 'google', 'facebook').
+ * @param {string} medium - The traffic medium (e.g., 'cpc', 'organic').
+ * @param {string} campaign - The campaign name.
+ * @returns {string} The channel grouping (e.g., 'Direct', 'Paid Search', 'Organic Social').
+ * 
+ */
+
+const getdefaultChannelGroupingSQL = (recordField) => {
+  // recordField should be "session_traffic_source_last_click"
+
+  const source = `
+    case
+      when ${recordField}.google_ads_campaign.campaign_name is not null
+           and ${recordField}.google_ads_campaign.campaign_name != '(not set)'
+        then 'google'
+      else ${recordField}.manual_campaign.source
+    end
+  `;
+
+  const medium = `
+    case
+      when ${recordField}.google_ads_campaign.campaign_name is not null
+           and ${recordField}.google_ads_campaign.campaign_name != '(not set)'
+        then 'cpc'
+      else ${recordField}.manual_campaign.medium
+    end
+  `;
+
+  const campaign = `
+    coalesce(${recordField}.google_ads_campaign.campaign_name,
+             ${recordField}.manual_campaign.campaign_name)
+  `;
+
+  return `
+    case
+      when lower(${source}) = '(direct)' and lower(${medium}) in ('(not set)', '(none)')
+        then 'Direct'
+
+      when regexp_contains(lower(${campaign}), r"cross-network")
+        then 'Cross-network'
+
+      when (regexp_contains(lower(${source}), r"(alibaba|amazon|google shopping|shopify|etsy|ebay|stripe|walmart)")
+            or regexp_contains(lower(${campaign}), r"(shop|shopping)"))
+           and regexp_contains(lower(${medium}), r"^(.*cp.*|ppc|retargeting|paid.*)$")
+        then 'Paid Shopping'
+
+      when regexp_contains(lower(${source}), r"(baidu|bing|duckduckgo|ecosia|google|yahoo|yandex)")
+           and regexp_contains(lower(${medium}), r"^(.*cp.*|ppc|retargeting|paid.*)$")
+        then 'Paid Search'
+
+      when regexp_contains(lower(${source}), r"(badoo|facebook|fb|instagram|linkedin|pinterest|tiktok|twitter|whatsapp)")
+           and regexp_contains(lower(${medium}), r"^(.*cp.*|ppc|retargeting|paid.*)$")
+        then 'Paid Social'
+
+      when regexp_contains(lower(${source}), r"(dailymotion|disneyplus|netflix|youtube|vimeo|twitch)")
+           and regexp_contains(lower(${medium}), r"^(.*cp.*|ppc|retargeting|paid.*)$")
+        then 'Paid Video'
+
+      when regexp_contains(lower(${medium}), r"(display|banner|expandable|interstitial|cpm)")
+        then 'Display'
+
+      when regexp_contains(lower(${source}), r"(alibaba|amazon|google shopping|shopify|etsy|ebay|stripe|walmart)")
+           or regexp_contains(lower(${campaign}), r"(shop|shopping)")
+        then 'Organic Shopping'
+
+      when regexp_contains(lower(${source}), r"(badoo|facebook|fb|instagram|linkedin|pinterest|tiktok|twitter|whatsapp)")
+           or regexp_contains(lower(${medium}), r"(social|social-network|social-media|sm|social network|social media)")
+        then 'Organic Social'
+
+      when regexp_contains(lower(${source}), r"(dailymotion|disneyplus|netflix|youtube|vimeo|twitch)")
+           or regexp_contains(lower(${medium}), r"(.*video.*)")
+        then 'Organic Video'
+
+      when regexp_contains(lower(${source}), r"(baidu|bing|duckduckgo|ecosia|google|yahoo|yandex)")
+           or lower(${medium}) = 'organic'
+        then 'Organic Search'
+
+      when regexp_contains(lower(${source}), r"(email|e-mail|e_mail|e mail)")
+           or regexp_contains(lower(${medium}), r"(email|e-mail|e_mail|e mail)")
+        then 'Email'
+
+      when lower(${medium}) = 'affiliate'
+        then 'Affiliates'
+
+      when lower(${medium}) in ('referral','app','link')
+        then 'Referral'
+
+      when lower(${medium}) = 'audio'
+        then 'Audio'
+
+      when lower(${medium}) = 'sms' or lower(${source}) = 'sms'
+        then 'SMS'
+
+      when regexp_contains(lower(${medium}), r"(push|mobile|notification)")
+           or lower(${source}) = 'firebase'
+        then 'Mobile Push Notifications'
+
+      when regexp_contains(lower(${medium}), r"(offline|out( |-|_)of( |-|_)home)")
+        then 'Offline'
+
+      when lower(${medium}) = 'print'
+        then 'Print'
+
+      else 'Unassigned'
+    end
+  `;
+};
 
 
 const helpers = {
@@ -720,7 +833,8 @@ const helpers = {
   generateAlterTableStatements,
   getSqlSelectEventsAsMetrics,
   getSqlPivotEventParams,
-  generateParamSQL
+  generateParamSQL,
+  getdefaultChannelGroupingSQL
 };
 
 module.exports = {
